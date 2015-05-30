42ea466a55d9bd23fde4670edc23c42dc334fd92
las93 <las92i@hotmail.fr>
2015-05-31 00:53:06 +0200

    Change the Demo public folder to Admin public folder + create the front public folder

--- public/Admin/index.php
+++ public/Admin/index.php
@@ -4,10 +4,10 @@
  * bootstrap of the demo
  *
  * @author    	Judicaël Paquet <judicael.paquet@gmail.com>
- * @copyright 	Copyright (c) 2013-2014 PAQUET Judicaël FR Inc. (https://github.com/las93)
- * @license   	https://github.com/las93/venus2/blob/master/LICENSE.md Tout droit réservé à PAQUET Judicaël
+ * @copyright 	Copyright (c) 2013-2015 PAQUET Judicaël FR Inc. (https://github.com/las93)
+ * @license   	https://github.com/las93/scrum/blob/master/LICENSE.md Tout droit réservé à PAQUET Judicaël
  * @version   	Release: 1.0.0
- * @filesource	https://github.com/las93/venus2
+ * @filesource	https://github.com/las93/scrum
  * @link      	https://github.com/las93
  * @since     	1.0
  */
@@ -15,7 +15,7 @@
 error_reporting(E_ALL);
 ini_set('display_error', 1);
 
-const PORTAIL = 'Demo';
+const PORTAIL = 'Admin';
 
 set_include_path(get_include_path().PATH_SEPARATOR.str_replace('public'.DIRECTORY_SEPARATOR.PORTAIL, 'private', __DIR__));
 
